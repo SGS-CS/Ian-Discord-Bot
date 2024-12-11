e958216ddf9fc0c3f4325fe33b7684ab3b3a1551
ian.qiu2025 <ian.qiu2025@stgeorges.bc.ca>
2024-12-10 21:44:36 -0800

    added one slash command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 import os
 import asyncio
@@ -9,6 +10,17 @@ bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 @bot.event
 async def on_ready():
     print("bot is online")
+    await bot.change_presence(activity=discord.Game(name='.help'))
+    try:
+        synced_commands = await bot.tree.sync()
+        print(f"Synced {len(synced_commands)} command(s).")
+    except Exception as e:
+        print("An error with syncing application commands has occured: ", e)
+
+#First Slash Command(will try to put them in a cog later)
+@bot.tree.command(name="hello", description="Says hello to the person who ran the command")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"{interaction.user.mention} Hello there!")
 
 with open("token.txt") as file:
     token = file.read()
